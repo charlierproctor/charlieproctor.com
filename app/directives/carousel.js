1f8ee8b596053c04cb3379f2d6c08da22f38dba5
Charlie Proctor <charlie@charlieproctor.com>
2015-07-12 23:01:36 -0400

    working on carousel directive

--- app/directives/carousel.js
+++ app/directives/carousel.js
@@ -1,14 +1,23 @@
 'use strict';
 
 angular.module('charlierproctor.carousel',[])
-.directive('carousel', function(){
+.directive('carousel', ['$interval',function($interval){
 
+	var i = 0;
+	var images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 	function link(scope, element, attrs) {
-		console.log("hi")
+		element.css("background","url(img/1.jpg) no-repeat center center fixed");
+		element.css("-webkit-background-size", "cover");
+		element.css("-moz-background-size", "cover");
+		element.css("-o-background-size", "cover");
+  		element.css("background-size", "cover");
+
+  		$interval(function(){
+  			element.css("background","url(img/" + images[++i%images.length] + ") no-repeat center center fixed");
+  		}, attrs['interval'])
 	}
 
 	return {
-		restrict: 'E',
 		link: link
 	}
-})
+}])
