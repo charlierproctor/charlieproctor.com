5215fad8603aa0c1bdf707028fc09e8057613b01
Charlie Proctor <charlie@charlieproctor.com>
2015-07-14 01:02:36 -0400

    pages abstract state. menu classes, etc. menu now shows on pages

--- app/pages/pages.js
+++ app/pages/pages.js
@@ -4,28 +4,36 @@ angular.module('charlierproctor.pages', ['ui.router'])
 
 .config(['$stateProvider',
 	function($stateProvider) {
-	  $stateProvider.state('about', {
+	  $stateProvider.state('pages', {
+	  	abstrast: true,
+	  	templateUrl: 'pages/pages.html',
+	  	controller: 'PagesCtrl'
+	  })
+	  .state('pages.about', {
 	    url: '/about',
 	    templateUrl: 'pages/about.html',
 	    controller: 'AboutCtrl'
 	  })
-	  .state('experience', {
+	  .state('pages.experience', {
 	  	url: '/experience',
 	    templateUrl: 'pages/experience.html',
 	    controller: 'ExperienceCtrl'
 	  })
-	  .state('code', {
+	  .state('pages.code', {
 	  	url: '/code',
 	    templateUrl: 'pages/code.html',
 	    controller: 'CodeCtrl'
 	  })
-	  .state('photography', {
+	  .state('pages.photography', {
 	  	url: '/photography',
 	    templateUrl: 'pages/photography.html',
 	    controller: 'PhotographyCtrl'
 	  })
 	}])
 
+.controller('PagesCtrl',['$scope',function($scope){
+
+}])
 .controller('AboutCtrl',['$scope',function($scope){
 
 }])
