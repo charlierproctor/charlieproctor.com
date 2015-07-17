a5b7973d8aff8eaecf9fe8633a575c24a3a2e692
Charlie Proctor <charlie@charlieproctor.com>
2015-07-16 21:03:42 -0400

    working on zoom directive

--- app/directives/zoom.js
+++ app/directives/zoom.js
@@ -3,12 +3,32 @@
 angular.module('charlierproctor.zoom',[])
 .directive('zoom', function(){
 
+	function flip(num){ return (num ? 0 : 1) }
 	function link(scope, element, attrs) {
-		element.append("<img src='img/photos/full/" + attrs.photo + "'>")
-	}
+		scope.current = 0
+		scope.imgs = []
+		scope.imgs.push(scope.photo)
 
+		scope.$watch('photo',function(newVal,oldVal){
+			if (newVal != oldVal){
+				angular.element(element.children()[scope.current]).removeClass('opaque')
+				scope.imgs[scope.current = flip(scope.current)] = newVal
+			}
+		})
+	}
 	return {
 		restrict: 'E',
-		link: link
+		link: link,
+		templateUrl: 'directives/zoom.html'
+	}
+})
+.directive('photo',function(){
+	return {
+		restrict: 'C',
+		link: function(scope, element, attrs){
+			element.on('load', function(){
+				element.addClass('opaque')
+			})
+		}
 	}
 })
