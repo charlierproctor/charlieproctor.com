173817b5cd686f126984e6ddcc34e62dcbdd46d3
Charlie Proctor <charlie@charlieproctor.com>
2015-07-17 03:32:29 -0400

    adding google analytics

--- app/app.js
+++ app/app.js
@@ -15,6 +15,12 @@ config(['$stateProvider','$urlRouterProvider',
 	function($stateProvider,$urlRouterProvider) {
 		$urlRouterProvider.otherwise("/");
 	}]).
-run(function($rootScope){
+run(['$rootScope','$location','$window',function($rootScope, $location, $window){
   $rootScope.images = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg"];
-})
+  $rootScope.$on('$stateChangeSuccess',function(event){
+    if (!$window.ga){
+      return;
+    }
+    $window.ga('send','pageview', { page:$location.path() })
+  })
+}])
