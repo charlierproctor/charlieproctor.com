694322a9b524cc0972696e79d895a184043ae1c6
Charlie Proctor <charlie@charlieproctor.com>
2015-07-17 22:43:40 -0400

    working on fs_list: recursively list files + directories in a directory

--- server.js
+++ server.js
@@ -6,10 +6,33 @@ var fs = require('fs')
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/app'));
 
-// list all files in a directory: /fs_list?dir=[directory]
+// recursively list files + directories in a directory: /fs_list?dir=[directory]
 app.get('/fs_list', function(req, res){
-	fs.readdir('app/'+req.query.dir, function(err,files){
-		res.send({"files":files})
+
+	var walk = function(dir,cb){
+		var hash = {"name":dir, "files":[], "directories":[]}
+		fs.readdir(dir, function(err,files){
+			var wait = files.length
+			for (var i = 0; i < files.length; i++) {
+				(function(filename,cb){
+					fs.stat(dir + '/' + filename, function(err,stats){
+						if (stats.isDirectory()){
+							walk(dir + '/' + filename,function(sub){
+								hash.directories.push(sub)
+								if (--wait === 0){ cb(hash) }
+							})
+						} else {
+							hash.files.push(filename)
+							if (--wait === 0){ cb(hash) }
+						}
+					})
+				})(files[i],cb)
+			};
+		})
+	}
+
+	walk('app/' + req.query.dir, function(hash){
+		res.send(hash)
 	})
 })
 
